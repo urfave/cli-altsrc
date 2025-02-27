ab4df8546ae528560184a6e2a2266171c975bffa
Naveen Gogineni <dear.chap@gmail.com>
2025-02-27 15:04:41 -0500

    Fix: Decouple cli interface from altsrc

--- yaml/yaml_value_source.go
+++ yaml/yaml_value_source.go
@@ -2,12 +2,11 @@ package yaml
 
 import (
 	altsrc "github.com/urfave/cli-altsrc/v3"
-	"github.com/urfave/cli/v3"
 	"gopkg.in/yaml.v3"
 )
 
 // YAML is a helper function to encapsulate a number of
 // yamlValueSource together as a cli.ValueSourceChain
-func YAML(key string, source altsrc.Sourcer) cli.ValueSource {
+func YAML(key string, source altsrc.Sourcer) *altsrc.ValueSource {
 	return altsrc.NewValueSource(yaml.Unmarshal, "yaml", key, source)
 }
