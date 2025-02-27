da04c90cbeb39b8d0892ea763d55e3169df10eb1
Naveen Gogineni <dear.chap@gmail.com>
2025-02-26 19:43:50 -0500

    Move to yaml v3

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/BurntSushi/toml v1.4.0
 	github.com/stretchr/testify v1.9.0
 	github.com/urfave/cli/v3 v3.0.0-beta1
-	gopkg.in/yaml.v2 v2.4.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- json/json_value_source.go
+++ json/json_value_source.go
@@ -3,7 +3,7 @@ package json
 import (
 	altsrc "github.com/urfave/cli-altsrc/v3"
 	"github.com/urfave/cli/v3"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 // JSON is a helper function that wraps the YAML helper function
